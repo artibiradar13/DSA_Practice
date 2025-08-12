ab67cf734c4e19c9623525088101103411e4b125
artibiradar13 <artibiradr4899@gmail.com>
2025-08-12 17:45:14 +0530

    remove duplicates

--- Two Pointers/Remove_Duplicates.java
+++ Two Pointers/Remove_Duplicates.java
@@ -4,23 +4,29 @@ public class Remove_Duplicates{
 
 
     public static int Remove(int[]arr){
+        //two pointers----slow,fast----
         int slow=0;
         if(arr.length==0) return 0;
         for(int fast=1;fast<arr.length;fast++){
             if(arr[fast]!=arr[slow]){
                 slow++;
                 arr[slow]=arr[fast];
+                
             }
         }return slow+1;
 
     }
 
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,4,5,5,5,6,6};
-        System.out.println(Remove(arr));
-        for (int i = 0; i < arr.length; i++) {
+        int[] arr={0,1,1,2,2,3,4,5,5,6};
+        int K=Remove(arr);
+        System.out.println(K);
+        for (int i = 0; i < K; i++) {
             System.out.print(arr[i]);
         }
+        //return each element appear at mst twice in final arr
+        int[] arr2={0,1,1,1,1,2,2,2,3,3,4};
+        if()
         
     }
 
